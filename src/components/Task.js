a310a7a35f5ba3a450376a27cf9c6e14031de6d9
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-18 14:41:26 +0200

    fixed task days

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,7 @@ const Task = ( {task} ) => {
     return (
         <div className='task'>
             <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} /></h3>
-            <p>task.day</p>
+            <p>{task.day}</p>
         </div>
     )
 }
